37500e668e5c43054bcaf216a1e79fa5c70e8163
jakegaxi <114057290+jakegaxi@users.noreply.github.com>
2023-02-16 22:08:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
+git clone https://gitee.com/wiwiz/WiFiPortal.git package
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
