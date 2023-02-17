d0b5b0533f7bfb93385a190fbec0561b87410542
jakegaxi <114057290+jakegaxi@users.noreply.github.com>
2023-02-17 20:58:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,12 +4,16 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
+# 解决ucl编译失败问题
+sed -i 's/configure/configure\ --build=alpha/g' ./tools/ucl/Makefile
+
+# 修复usign导致编译报错
+sed -i '/usign/d'  package/Makefile
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-git clone https://gitee.com/wiwiz/WiFiPortal.git immortalwrtARM/package/
+git clone https://gitee.com/wiwiz/WiFiPortal.git openwrt/package/
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
